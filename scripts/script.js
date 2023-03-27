ca36d251a90221abeac849380fe83efbd3f2f878
Andrew <ahuang84@my.bcit.ca>
2023-03-27 10:47:02 -0700

    Multiple EventListener

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,26 @@
 const buttonRef = document.querySelector("button")
-console.log(buttonRef)
+const bodyRef = document.querySelector("body")
+
 
 function alertUser(){
     alert("You clicked the button!")
 }
 
+function changeBGPink(){
+    // bodyRef.style.backgroundColor = "pink"
+    bodyRef.classList.add("pinkBG")
+}
+
+function changeText(){
+
+    if (buttonRef.textContent === "Click Me!"){
+        buttonRef.textContent = "Clicked!"
+    }
+    else{
+        buttonRef.textContent = "Click Me!"
+    }
+}
+
 buttonRef.addEventListener("click", alertUser, {once: true})
+buttonRef.addEventListener("click", changeBGPink)
+buttonRef.addEventListener("click", changeText)
