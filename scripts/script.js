1dda12cf22f98acbda4b932a36a6a6ea5d854adf
Andrew <ahuang84@my.bcit.ca>
2023-03-27 11:17:13 -0700

    Starting Bubbling Phase Activity

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,45 @@
-const buttonRef = document.querySelector("button")
-const bodyRef = document.querySelector("body")
+// const buttonRef = document.querySelector("button")
+// const bodyRef = document.querySelector("body")
 
 
-function alertUser(){
-    alert("You clicked the button!")
-}
+// function alertUser(){
+//     alert("You clicked the button!")
+// }
 
-function changeBGPink(){
-    // bodyRef.style.backgroundColor = "pink"
-    bodyRef.classList.add("pinkBG")
-}
+// function changeBGPink(){
+//     // bodyRef.style.backgroundColor = "pink"
+//     bodyRef.classList.add("pinkBG")
+// }
 
-function changeText(){
+// function changeText(){
 
-    if (buttonRef.textContent === "Click Me!"){
-        buttonRef.textContent = "Clicked!"
+//     if (buttonRef.textContent === "Click Me!"){
+//         buttonRef.textContent = "Clicked!"
+//     }
+//     else{
+//         buttonRef.textContent = "Click Me!"
+//     }
+// }
+
+// buttonRef.addEventListener("click", alertUser, {once: true})
+// buttonRef.addEventListener("click", changeBGPink)
+// buttonRef.addEventListener("click", changeText)
+
+// function updateImage(event){
+//     const image = document.querySelector("#shoppingCart")
+//     image.setAttribute("src","images/shoppingCart.png")
+//     image.setAttribute("alt", "Image of shopping cart")
+//     image.setAttribute("height", "75")
+//     image.setAttribute("width", "75")
+// }
+
+// buttonRef.addEventListener("click", updateImage)
+
+const buttonContainerRef = document.querySelector(".button-container")
+function bgChange(event){
+    if (event.target.tagName === "BUTTON"){
+        event.target.classList.add("greenBG")
     }
-    else{
-        buttonRef.textContent = "Click Me!"
-    }
+
 }
-
-buttonRef.addEventListener("click", alertUser, {once: true})
-buttonRef.addEventListener("click", changeBGPink)
-buttonRef.addEventListener("click", changeText)
-
-function updateImage(){
-    const image = document.querySelector("#shoppingCart")
-    image.setAttribute("src","images/shoppingCart.png")
-    image.setAttribute("alt", "Image of shopping cart")
-    image.setAttribute("height", "75")
-    image.setAttribute("width", "75")
-}
-
-buttonRef.addEventListener("click", updateImage)
+buttonContainerRef.addEventListener("mouseover",bgChange)
